e672c0931ff962e28071a8d1ffaeec98828e9c7d
Ali Hamrani <ali.hamrani2001@gmail.com>
2024-03-05 11:15:33 +0330

    fix error pages bugs

--- internal/handler/error.go
+++ internal/handler/error.go
@@ -2,16 +2,17 @@ package handler
 
 var (
 	NotFoundFiles = []string{
+		"internal/template/base/base.html",
 		"internal/template/page/404.html",
 		"internal/template/static/header.html",
-		"internal/template/static/footer.html",
-		"internal/template/static/head.html",
+		"internal/template/assets/links.html",
+		"internal/template/static/header.html",
 	}
 
 	InternalServerErrorFiles = []string{
+		"internal/template/base/base.html",
 		"internal/template/page/500.html",
+		"internal/template/assets/links.html",
 		"internal/template/static/header.html",
-		"internal/template/static/footer.html",
-		"internal/template/static/head.html",
 	}
 )
